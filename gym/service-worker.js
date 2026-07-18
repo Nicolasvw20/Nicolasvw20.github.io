5aed14b4764fba96e31cf5768395f57af2b11309
Nicolasvw20 <revolvw@googlemail.com>
2026-07-18 16:00:13 +0100

    Refresh Form Performance offline cache

--- gym/service-worker.js
+++ gym/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'form-performance-v2';
-const ASSETS = ['./','index.html','app.js','manifest.webmanifest','icon.svg'];
+const CACHE = 'form-performance-v3';
+const ASSETS = ['./','index.html','app.js','app-part-1.txt','app-part-2.txt','app-part-3.txt','app-part-4.txt','app-part-5.txt','app-part-6.txt','manifest.webmanifest','icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {

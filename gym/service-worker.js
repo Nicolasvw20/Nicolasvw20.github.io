e4bbed432aa796a98a926ff0f1e10066d9e358a8
Nicolasvw20 <revolvw@googlemail.com>
2026-07-18 15:54:41 +0100

    Upgrade Form Performance offline support

--- gym/service-worker.js
+++ gym/service-worker.js
@@ -1,1 +1,10 @@
-const C='ppl-gym-v1',A=['./','index.html','app.js','manifest.webmanifest','icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('index.html')))));
+const CACHE = 'form-performance-v2';
+const ASSETS = ['./','index.html','app.js','manifest.webmanifest','icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('index.html'))));
+});
